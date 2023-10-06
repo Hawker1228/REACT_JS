0bc297de6bd7669fe594530d1952b7d59ee08b09
Hawker1228 <99539292+Hawker1228@users.noreply.github.com>
2023-10-06 16:00:51 +0300

    15:56 UPDATE 06.10.23 Rechange const "App" to Class "App" Rechange const "Header" to Class "Header" move events "inputClick" and "mouseOver" to class App

--- client/src/index.js
+++ client/src/index.js
@@ -4,41 +4,38 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Logo from './Logo';
 
-
-const inputClick = () => console.log("Clicked");
-const mouseOver = () => console.log("Mouse Over");
-const helpText = "Help text!";
-
-const element = ( <div className='elem_1'>
-    <h1>{helpText}</h1><input placeholder={helpText} 
-    onClick={inputClick} onMouseEnter={mouseOver}/>
-    <p>{helpText === "Help text!" ? "Yes" : "No"}</p>
-</div>);
-
 class Header extends React.Component {
   render() {
     const { a, children } = this.props;
   return (
 <header className={a}>
-  <div>
-  {children}
-  </div>
+  <div>{children}</div>
 </header>
   );
 }
 }
 
-const App = () => {
-  return (
-    <div>
-    <div className="App">
-       <Header a={'App-header'}><Logo><a>React  App</a></Logo>
-       <div className='div_nav'><a href='#'>HELLO </a><a href='#'>WE HERE </a><a href='#'> ABOUT</a></div>
-       </Header> 
-    </div>
-    {element}
-    </div>
-  );
+class App extends React.Component {
+  helpText = "Help text!";
+  render() {
+    return (
+      <div>
+      <div className="App">
+         <Header a={'App-header'}><Logo><a href='./index.js'>React  App</a></Logo>
+
+          <div className='div_nav'><a href='./index.js'>HELLO </a><a href='./index.js'>WE HERE </a><a href='./index.js'> ABOUT</a></div>
+         </Header> 
+      </div>
+        <div className='elem_1'>
+          <h1>{this.helpText}</h1><input placeholder={this.helpText} 
+          onClick={this.inputClick} onMouseEnter={this.mouseOver}/>
+          <p>{this.helpText === "Help text!" ? "Yes" : "No"}</p>
+        </div>
+      </div>
+    )
+  }
+  inputClick() {console.log("Clicked")}
+  mouseOver() {console.log("Mouse over")}
 }
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
