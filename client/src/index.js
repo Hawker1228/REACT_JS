ce26049b4eb24ff68f693a4871c59d315ae888a7
Hawker1228 <99539292+Hawker1228@users.noreply.github.com>
2023-10-11 17:02:56 +0300

    17:01 UPDATE 11.10.23 I am add new font in my project Continue learn  how JSON data working in React JS

--- client/src/index.js
+++ client/src/index.js
@@ -12,50 +12,64 @@ class Header extends React.Component {
     const { a, children } = this.props;
   return (
 <header className={a}>
+  
   <div>{children}</div>
 </header>
   );
 }
 }
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      helpText: "Welcome",
-      userData: ""
-    }
-    this.inputClick = this.inputClick.bind(this) //СВЯЗЫВАЕМ Input.Click с конструктором
-  }
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       helpText: "Welcome",
+//       userData: ""
+//     }
+//     this.inputClick = this.inputClick.bind(this) //СВЯЗЫВАЕМ Input.Click с конструктором
+//   }
 
-  render() {let test_id = 'p_test';
+//   render() {let test_id = 'p_test';
+//     return (
+//       <div className="App">
+//         <Header a={'App-header'}><Logo><p>React  App</p></Logo>
+//           <div className='div_nav'><a href='./index.js'>HELLO </a><a href='./index.js'>WE HERE </a><a href='./index.js'> ABOUT</a></div>
+//         </Header> 
+//          <div className='elem_1'>
+//           <h1>{this.state.helpText}</h1>
+//           <h2>{this.state.userData}</h2> 
+//           <input placeholder={this.state.helpText}
+//             onChange={event => this.setState({ userData: event.target.value })} //Отслеживаем изменение placeholder  и меняем значение userData
+//             onClick={this.inputClick} onMouseEnter={this.mouseOver}/>
+//           <p>{this.state.helpText === "Welcome" ? "Yes" : "No"}</p>
+//           <p id='p_test'>0</p>
+//           <Button p_id={test_id} text='Увеличить'></Button>
+//           </div>
+//           <br />
+
+//           <JsonParse></JsonParse> {/* ЗАГРУЖАЕМ КОМПОНЕНТ С ОТРИСОВКОЙ JSON */}
+//       </div>
+//     )
+//   }
+//   inputClick() {
+//     this.setState({helpText: "Changed"}) //УСТАНАВЛИВАЕМ СОСТОЯНИЕ
+//     console.log("Clicked")
+//   }
+//   mouseOver() {console.log("Mouse over")}
+// }
+
+
+class App extends React.Component {
+  render() {
     return (
       <div className="App">
-        <Header a={'App-header'}><Logo><p>React  App</p></Logo>
-          <div className='div_nav'><a href='./index.js'>HELLO </a><a href='./index.js'>WE HERE </a><a href='./index.js'> ABOUT</a></div>
-        </Header> 
-         <div className='elem_1'>
-          <h1>{this.state.helpText}</h1>
-          <h2>{this.state.userData}</h2> 
-          <input placeholder={this.state.helpText}
-            onChange={event => this.setState({ userData: event.target.value })} //Отслеживаем изменение placeholder  и меняем значение userData
-            onClick={this.inputClick} onMouseEnter={this.mouseOver}/>
-          <p>{this.state.helpText === "Welcome" ? "Yes" : "No"}</p>
-          <p id='p_test'>0</p>
-          <Button p_id={test_id} text='Увеличить'></Button>
-          </div>
-          <br />
-
-          <JsonParse></JsonParse> {/* ЗАГРУЖАЕМ КОМПОНЕНТ С ОТРИСОВКОЙ JSON */}
+        <Header title="Список пользователей" a={'App-header'}><Logo><p>React  App</p></Logo>
+        <div className='div_nav'><a href='./index.js'>HELLO </a><a href='./index.js'>WE HERE </a><a href='./index.js'> ABOUT</a></div>
+        </Header>
+        <JsonParse></JsonParse> {/* ЗАГРУЖАЕМ КОМПОНЕНТ С ОТРИСОВКОЙ JSON */}
       </div>
     )
   }
-  inputClick() {
-    this.setState({helpText: "Changed"}) //УСТАНАВЛИВАЕМ СОСТОЯНИЕ
-    console.log("Clicked")
-  }
-  mouseOver() {console.log("Mouse over")}
 }
-
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 root.render(
